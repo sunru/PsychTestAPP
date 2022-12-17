893cd3da33a874dc790730306d63130465a14bf2
RubySun <sunru10075@gmail.com>
2022-12-17 21:27:36 +0800

    Initial Commit

---
+++ PsychTestAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PsychTestAPP
+//
+//  Created by 廖晨如 on 2022/12/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
